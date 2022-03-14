c0ce6cf4a459e8f8abd23cf0767d42b1d49dbf50
gobindasarkaritech <gobindasarkar@itech-softsolutions.com>
2022-03-14 10:22:28 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,267 @@
+(function($) {
+  "use strict";
+  /*-------------------------------------------
+  preloader active
+  --------------------------------------------- */
+  jQuery(window).on('load',function() {
+    jQuery('.preloader').fadeOut('slow');
+  });
+
+  /*-------------------------------------------
+  Sticky Header
+  --------------------------------------------- */
+  $(window).on('scroll', function(){
+    if( $(window).scrollTop()>80 ){
+      $('#sticky').addClass('stick');
+    } else {
+      $('#sticky').removeClass('stick');
+    }
+  }); 
+  
+  jQuery(document).ready(function(){
+    /*-------------------------------------------
+    js scrollup
+    --------------------------------------------- */
+    $.scrollUp({
+      scrollText: '<i class="fa fa-angle-up"></i>',
+      easingType: 'linear',
+      scrollSpeed: 900,
+      animation: 'fade'
+    }); 
+    /*-------------------------------------------
+    topbar-slide active
+    --------------------------------------------- */
+    $('.topbar-slide').slick({
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      dots: false,
+      arrows: true,
+      fade: true,
+      prevArrow: '<i class="slick-prev fas fa-angle-left"></i> ',
+      nextArrow: '<i class="slick-next fas fa-angle-right"></i> ',
+    });
+    /*-------------------------------------------
+    brand-slide active
+    --------------------------------------------- */
+    $('.brand-slide').slick({
+      infinite: true,
+      speed: 500,
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      dots: false,
+      arrows: false,
+      variableWidth: true,
+      prevArrow: '<i class="slick-prev fas fa-angle-left"></i> ',
+      nextArrow: '<i class="slick-next fas fa-angle-right"></i> ',
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 5,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+    /*-------------------------------------------
+    company-slide active
+    --------------------------------------------- */
+    $('.company-slide').slick({
+      infinite: true,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      centerMode: true,
+      dots: false,
+      arrows: false,
+      prevArrow: '<i class="slick-prev fas fa-angle-left"></i> ',
+      nextArrow: '<i class="slick-next fas fa-angle-right"></i> ',
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+    /*-------------------------------------------
+    image-slide active
+    --------------------------------------------- */
+    $('.image-slide').slick({
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      dots: true,
+      arrows: false,
+      prevArrow: '<i class="slick-prev arrow fas fa-angle-left"></i> ',
+      nextArrow: '<i class="slick-next arrow fas fa-angle-right"></i> ',
+    });
+    /*-------------------------------------------
+    conversation-list active
+    --------------------------------------------- */
+    $('.conversation-list').slick({
+      infinite: true,
+      speed: 500,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      centerMode: false,
+      dots: false,
+      arrows: false,
+      prevArrow: '<i class="slick-prev arrow fas fa-angle-left"></i> ',
+      nextArrow: '<i class="slick-next arrow fas fa-angle-right"></i> ',
+      vertical: false,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+    /*-------------------------------------------
+    slider active
+    --------------------------------------------- */
+    $('.slider-active').slick({
+      infinite: true,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      centerMode: false,
+      dots: false,
+      arrows: true,
+      prevArrow: '<i class="slick-prev arrow fas fa-angle-left"></i> ',
+      nextArrow: '<i class="slick-next arrow fas fa-angle-right"></i> ',
+      vertical: false,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+    /*-------------------------------------------
+    js counterUp
+    --------------------------------------------- */
+    $('.counter').counterUp({
+      delay: 10,
+      time: 1000
+    });
+
+    /*---------------------------------
+    venobox Popup active
+    -----------------------------------*/
+    $('.popup-video').venobox(); 
+
+    /*---------------------------------
+    isotope activation 
+    -----------------------------------*/
+    // filter items on button click
+    $('.filtering-button').on('click', 'a', function () {
+      var filterValue = $(this).attr('data-filter');
+      $grid.isotope({ filter: filterValue });
+    });
+    var $grid = $('.grid').isotope({
+      // set itemSelector so .grid-sizer is not used in layout
+      itemSelector: '.grid-item',
+      percentPosition: true,
+      animationOptions: {
+        duration: 500,
+        easing: 'zoom-in'
+      },
+      masonry: {
+        // use element for option
+        columnWidth: '.grid-item'
+      },
+      transitionDuration: '.9s'
+    })
+  
+    $('.filtering-button a').on('click',  function () {
+      $('.filtering-button a').removeClass('active');
+      $(this).addClass('active');
+    });
+
+    
+
+  });
+
+})(jQuery);
