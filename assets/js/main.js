af2308be73f14e7f79eca33b6fc47e5b9a024028
gobindasarkaritech <gobindasarkar@itech-softsolutions.com>
2022-03-14 19:43:38 +0600

    shop page done single shop page working progress

--- assets/js/main.js
+++ assets/js/main.js
@@ -178,36 +178,32 @@
       ]
     });
     /*-------------------------------------------
-    slider active
+    single-product-slide active
     --------------------------------------------- */
-    $('.slider-active').slick({
+    $('.single-product-slide').slick({
       infinite: true,
       speed: 500,
-      slidesToShow: 3,
+      slidesToShow: 2,
       slidesToScroll: 1,
       autoplay: true,
       autoplaySpeed: 3000,
-      centerMode: false,
-      dots: false,
+      dots: true,
       arrows: true,
-      prevArrow: '<i class="slick-prev arrow fas fa-angle-left"></i> ',
-      nextArrow: '<i class="slick-next arrow fas fa-angle-right"></i> ',
-      vertical: false,
+      prevArrow: '<i class="slick-prev fas fa-angle-left"></i> ',
+      nextArrow: '<i class="slick-next fas fa-angle-right"></i> ',
       responsive: [
         {
           breakpoint: 1024,
           settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true
+            slidesToShow: 2,
+            slidesToScroll: 1,
           }
         },
         {
           breakpoint: 600,
           settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
+            slidesToShow: 1,
+            slidesToScroll: 1
           }
         },
         {
@@ -218,6 +214,24 @@
           }
         }
       ]
+    });
+    /*-------------------------------------------
+    Product Quantity JS
+    --------------------------------------------- */
+    $('.qty-btn').on('click', function (e) {
+      e.preventDefault();
+      var $button = $(this);
+      var oldValue = $button.parent().find('input').val();
+      if ($button.hasClass('inc')) {
+        var newVal = parseFloat(oldValue) + 1;
+      } else {
+        if (oldValue > 1) {
+          var newVal = parseFloat(oldValue) - 1;
+        } else {
+          newVal = 1;
+        }
+      }
+      $button.parent().find('input').val(newVal);
     });
     /*-------------------------------------------
     js counterUp
