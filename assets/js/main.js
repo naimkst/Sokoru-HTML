fa5b951a60456fc6337378823e3f8b67fc7252c7
gobindasarkaritech <gobindasarkar@itech-softsolutions.com>
2022-03-28 14:09:56 +0600

    all page and change done

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,6 +30,11 @@
   }); 
   
   jQuery(document).ready(function(){
+    /*---------------------------------
+    AOS active
+    -----------------------------------*/
+    AOS.init();
+
     // mobile dropdown menu 
     $('.mobile-menu .has-sub-menu .dropdown-btn').on('click', function() {
       $(this).siblings('.sub-menu').slideToggle();
@@ -220,21 +225,21 @@
         {
           breakpoint: 1024,
           settings: {
-            slidesToShow: 3,
+            slidesToShow: 4,
             slidesToScroll: 1,
           }
         },
         {
           breakpoint: 600,
           settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
+            slidesToShow: 4,
+            slidesToScroll: 1
           }
         },
         {
           breakpoint: 480,
           settings: {
-            slidesToShow: 1,
+            slidesToShow: 4,
             slidesToScroll: 1
           }
         }
@@ -328,7 +333,7 @@
     plastic-product-slidep active
     --------------------------------------------- */
     $('.plastic-product-slide').slick({
-      infinite: true,
+      infinite: false,
       speed: 500,
       slidesToShow: 3,
       slidesToScroll: 1,
@@ -363,7 +368,7 @@
         {
           breakpoint: 480,
           settings: {
-            slidesToShow: 1,
+            slidesToShow: 1.3,
             slidesToScroll: 1,
           }
         }
@@ -373,11 +378,11 @@
     product-slide active
     --------------------------------------------- */
     $('.product-slide').slick({
-      infinite: true,
+      infinite: false,
       speed: 500,
       slidesToShow: 3,
       slidesToScroll: 1,
-      autoplay: true,
+      autoplay: false,
       autoplaySpeed: 3000,
       dots: false,
       arrows: false,
@@ -408,8 +413,9 @@
         {
           breakpoint: 480,
           settings: {
-            slidesToShow: 1,
+            slidesToShow: 1.3,
             slidesToScroll: 1,
+            centerMode: false
           }
         }
       ]
@@ -444,7 +450,7 @@
     venobox Popup active
     -----------------------------------*/
     $('.popup-video').venobox(); 
-
+    
 
     
 
